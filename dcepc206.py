9bb948e4ae96e05f790badda21cba6b96c55e7c8
Rafael Cunha de Almeida <rafael.almeida@ef.com>
2016-12-18 22:29:26 +0000

    Use a binary tree

--- dcepc206.py
+++ dcepc206.py
@@ -14,29 +14,86 @@
 # Input
 
 # First line gives T, number of test cases.
-
 # 2T lines follow.
 # First line gives you the number of stairs N
 # Next line gives you N numbers written on the stairs.
 
 # Output
-# 
+
 # For each test case output one line giving the final sum for each test case.
 
 # Constraints
+
 # T<=10
 # 1<=N<=10^5
 
 # All numbers will be between 0 and 10^6.
 
-def add_filter(list_, limit):
-    return sum(x for x in list_ if x < limit)
+import sys
+
+class BinaryTree:
+
+    def __init__(self, v):
+        self.left = None
+        self.right = None
+        self.value = v
+        self.sum = 0
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return "[{} {}] ({}) ({})".format(self.value, self.sum, self.left, self.right)
+
+    def insert(self, v):
+        current = self
+        tree = BinaryTree(v)
+        s = 0
+        while True:
+            if current.value == v:
+                break
+            elif current.value < v:
+                if current.right == None:
+                    tree.sum += current.sum + current.value
+                    current.right = tree
+                    break
+                else:
+                    s += current.sum + current.value
+                    current = current.right
+            else:
+                current.sum += v
+                if current.left == None:
+                    tree.sum = s
+                    current.left = tree
+                    break
+                else:
+                    current = current.left
+
+    def find(self, v):
+        current = self
+        while True:
+            if current.value == v:
+                return current
+            elif current.value < v:
+                if current.right != None:
+                    current = current.right
+                else:
+                    return None
+            else:
+                if current.left != None:
+                    current = current.left
+                else:
+                    return None
 
 
 def solve(stair):
+    tree = BinaryTree(stair.pop(0))
     notes = []
-    for i, step in enumerate(stair):
-        notes.append(add_filter(stair[:i], step))
+    for step in stair:
+        tree.insert(step)
+        elem = tree.find(step)
+        #print(step, elem.sum, tree)
+        notes.append(elem.sum)
     return sum(notes)
 
 
